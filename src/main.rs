45485a05aa18911b79244c4d2c9a03928fd52118
Joshua Mo <joshua.mo.876@gmail.com>
2024-07-24 14:26:15 +0100

    fix: no cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use axum::{response::IntoResponse, routing::{get, post}, Extension, Json, Router};
 use serde::{Deserialize, Serialize};
+use tower_http::services::ServeDir;
 use shuttle_openai::async_openai::config::OpenAIConfig;
-use shuttle_openai::async_openai::types::CreateCompletionRequestArgs;
+use shuttle_openai::async_openai::types::{CreateChatCompletionRequestArgs, ChatCompletionRequestUserMessageArgs};
 use shuttle_openai::async_openai::Client;
 
 async fn hello_world() -> &'static str {
@@ -13,36 +14,38 @@ async fn main(#[shuttle_openai::OpenAI(
     api_key="{secrets.OPENAI_API_KEY}"
 )] cfg: Client<OpenAIConfig>) -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
-        .route("/", get(hello_world))
         .route("/prompt", post(prompt))
-        .layer(Extension(cfg));
+        .layer(Extension(cfg))
+        .nest_service("/", ServeDir::new("static"));
 
     Ok(router.into())
 }
 
 #[derive(Deserialize, Serialize)]
 struct Prompt {
-    inner: String,
+    prompt: String,
 }
 
 async fn prompt(
     Extension(openai): Extension<Client<OpenAIConfig>>,
     Json(prompt): Json<Prompt>,
 ) -> Result<impl IntoResponse, impl IntoResponse> {
-    let request = match CreateCompletionRequestArgs::default()
+    let request = CreateChatCompletionRequestArgs::default()
+        .max_tokens(512u32)
         .model("gpt-4o")
-        .prompt(prompt.inner)
-        .max_tokens(40_u32)
-        .build()
-    {
-        Ok(req) => req,
-        Err(e) => return Err(e.to_string()),
-    };
+        .messages([
+            ChatCompletionRequestUserMessageArgs::default()
+                .content(prompt.prompt)
+                .build().unwrap()
+                .into()
+        ])
+        .build().unwrap();
 
-    let response = match openai.completions().create(request).await {
+
+    let response = match openai.chat().create(request).await {
         Ok(res) => res,
         Err(e) => return Err(e.to_string()),
     };
 
-    Ok(response.choices.into_iter().next().unwrap().text)
+    Ok(response.choices.into_iter().next().unwrap().message.content.unwrap())
 }
